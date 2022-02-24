409771de45d9c87dc7072bda14d14aeaa7078b6d
Matt3o0 <83077061+duvbolone@users.noreply.github.com>
2022-02-24 16:42:38 +0100

    Created a py script
    A script with experimenting with the sleep function from the time lib

---
+++ Sleep.py
@@ -0,0 +1,12 @@
+variabl1 = 5
+variabl2= 10
+variabl3= 15
+
+variabl4= 5*7
+
+print(variabl1+variabl2+variabl3+variabl4)
+print("python is just fast lua")
+
+from time import sleep
+sleep(2)   
+print("i like sleep")
